924abb45cf33fc0e6343f07888a6bb6d0111d817
Alexei Escorcia Macías <alexei377.2017@gmail.com>
2025-05-22 00:31:48 -0600

    Comment reactions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :comment_reactions, dependent: :destroy
+  has_many :liked_comments, through: :comment_reactions, source: :comment
+
 
   validates :title, presence: true
   validates :content, presence: true
