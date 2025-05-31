f1f1e2c0a16a9bf712c029fbacd56c76708aa6d1
Alexei Escorcia Macías <alexei377.2017@gmail.com>
2025-05-30 22:41:51 -0600

    Deploy on Render with CockroachDB

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[8.0].define(version: 2025_05_30_055955) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
 
   create_table "comment_reactions", force: :cascade do |t|
     t.bigint "user_id", null: false
